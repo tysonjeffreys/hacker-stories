a074fabfd115c5a6f735c8894ff7fc6a65624cfc
Tyson <tysonjeffreys@gmail.com>
2021-01-18 19:35:17 -0700

    Creating lists with map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+const welcome = {
+  greeting: 'Hey',
+  title: 'Hyperion'
+};
+
+const anArray = [1, 2, 3, 7, 7, 7]
+
+function getTitle(title) {
+  return title;
+}
+
+
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.js.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>'My Hacker Stories'</h1>
+
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    <hr />
+      {list.map(function(item) {
+        return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span> {item.author} </span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+        );                                              
+      })}
+
     </div>
   );
 }
